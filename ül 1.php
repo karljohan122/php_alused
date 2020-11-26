3e38b12bbad89c6f7750f606f18f0e7999ea4695
karljohan122 <karljohan122@gmail.com>
2020-11-26 10:50:41 +0200

    Ül 1

---
+++ ül 1.php
@@ -0,0 +1,7 @@
+<?php #Ül 1 Karl Johan Linnas 26.11.2020
+$nimi = "Karl";
+$synniaasta = "10.01.2000";
+$tahtkuju = "Kaljukits";
+echo "$nimi <br> $synniaasta <br> $tahtkuju <br>";
+echo "It's My Life - Dr. Alban <br>";
+echo "(\(\ <br> ( -.-) <br> o_('')('') ";
